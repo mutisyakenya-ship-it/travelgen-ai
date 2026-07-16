999d8f65d558683cef36b31de7c2047aff8a46e5
Ambrose mutisya <mutisyakenya@gmail.com>
2026-07-16 07:29:52 -0700

    feat: remove Font Awesome dependency from package.json and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import {createRoot} from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import { AuthProvider } from "./contexts/AuthContext";
 
 import App from "./App";
